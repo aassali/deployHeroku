6f80bb1fa680e15b55670553e0a057fa674d2eb7
Antoine <assali.antoine@gmail.com>
2019-09-21 19:25:21 +0200

    async fnct

--- src/modules/users/services/updateOneById.js
+++ src/modules/users/services/updateOneById.js
@@ -5,30 +5,23 @@ const connect = require('../../../clients/mongodb');
 const collections = require('../../../enums/collections');
 const findOneById = require('./findOneById');
 
-module.exports = (id, userToUpdate) => {
-  return updateModel.validate(userToUpdate)
-    .then(() => {
-      if (userToUpdate.password) {
-        const user = {
-          ...userToUpdate,
-          password: bcrypt.hashSync(userToUpdate.password, 10),
-        };
-        return user;
-      }
-      return userToUpdate;
-    })
-    .then((user) => {
-      return connect()
-        .then(db => db.collection(collections.USERS))
-        .then(collection => collection.updateOne({ _id: ObjectId(id) }, { $set: user }))
-        .then((dbResponse) => {
-          if (dbResponse.matchedCount === 1) {
-            return findOneById(id);
-          }
-
-          const err = new Error('Not Found');
-          err.status = 404;
-          throw err;
-        });
-    });
+module.exports = async (id, userToUpdate) => {
+  await updateModel.validate(userToUpdate);
+  if (userToUpdate.password) {
+    const user = {
+      ...userToUpdate,
+      password: bcrypt.hashSync(userToUpdate.password, 10),
+    };
+    return user;
+  }
+  const user_1 = userToUpdate;
+  const db = await connect();
+  const collection = db.collection(collections.USERS);
+  const dbResponse = collection.updateOne({ _id: ObjectId(id) }, { $set: user_1 });
+  if (dbResponse.matchedCount === 1) {
+    return findOneById(id);
+  }
+  const err = new Error('Not Found');
+  err.status = 404;
+  throw err;
 };
